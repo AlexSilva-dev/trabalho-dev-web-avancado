9bd2a29a5e0109fb21fe174160fa802cacd95e2a
AlexSilva <alexaleq321@gmail.com>
2023-10-28 18:45:04 -0300

    salvando projeto

---
+++ trabalho-dev-web-avancado/src/main/java/com/uff/trabalhodevwebavancado/TrabalhoDevWebAvancadoApplication.java
@@ -0,0 +1,13 @@
+package com.uff.trabalhodevwebavancado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoDevWebAvancadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoDevWebAvancadoApplication.class, args);
+	}
+
+}
---
+++ trabalho-dev-web-avancado/src/test/java/com/uff/trabalhodevwebavancado/TrabalhoDevWebAvancadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uff.trabalhodevwebavancado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoDevWebAvancadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
