467252bcd76216301e7369c014a0e6567b957cb4
AlexSilva <alexaleq321@gmail.com>
2023-12-16 20:35:45 -0300

    space arrumado

--- api/src/main/java/br/com/projeto/api/controle/ActivitiesController.java
+++ api/src/main/java/br/com/projeto/api/controle/ActivitiesController.java
@@ -18,51 +18,56 @@ public class ActivitiesController {
     private ActivitiesRepository activitiesRepository;
 
     @PostMapping("/editions/{editionId}/activities")
-    public Activities save(@PathVariable int editionId,@RequestBody Activities a) {
-        Edition edition = editionRepository.findById(editionId);
+    public Activities save(@RequestBody Activities a) {
+        public Activities save ( @PathVariable int editionId, @RequestBody Activities a){
+            Edition edition = editionRepository.findById(editionId);
 
-        if (edition != null) {
-            a.setEdition(edition); // Configura a edição na atividade
-            edition.getAtividades().add(a); // Adiciona a atividade à lista de atividades da edição
-            editionRepository.save(edition); // Atualiza a edição no banco de dados
+            if (edition != null) {
+                a.setEdition(edition); // Configura a edição na atividade
+                edition.getAtividades().add(a); // Adiciona a atividade à lista de atividades da edição
+                editionRepository.save(edition); // Atualiza a edição no banco de dados
+            }
+            return activitiesRepository.save(a);
         }
-        return activitiesRepository.save(a);
-    }
 
-    //pecriso do metodo de edition que pega a lista de atividades de uma edition
-    @GetMapping("/editions/{editionId}/activities")
-    public Iterable<Activities> searchAll() {
-        return activitiesRepository.findAll();
-    }
-
-    //pecriso do metodo de edition para pegar users
-    @GetMapping("/editions/{editionId}/activities/{activitiesId}")
-    public Activities searchUser(@PathVariable int activitiesId) {
-        return activitiesRepository.findById(activitiesId);
-    }
-
-    @PutMapping("/editions/{editionId}/activities/{activitiesId}")
-    public Activities update(@PathVariable int activitiesId, @RequestBody Activities a) {
-        Activities activities = activitiesRepository.findById(activitiesId);
-        activities.setName(a.getName());
-        //activities.setType(a.getType());
-        activities.setDescription(a.getDescription());
-        activities.setDate(a.getDate());
-        activities.setStart_time(a.getStart_time());
-        activities.setEnd_time(a.getEnd_time());
-
-        return activitiesRepository.save(activities);
-    }
-
-    @DeleteMapping("/editions/{editionId}/activities/{activitiesId}")
-    public void delete(@PathVariable int editionId,@PathVariable int activitiesId) {
-        Activities a = activitiesRepository.findById(activitiesId)
-        Edition edition = findEditionofActivities(a);
-        if (edition != null) {
-            edition.getAtividades().remove(a); // Remove a atividade da lista de atividades da edição
-            editionRepository.save(edition); // Atualiza a edição no banco de dados
+        //pecriso do metodo de edition que pega a lista de atividades de uma edition
+        @GetMapping("/editions/{editionId}/activities")
+        public Iterable<Activities> searchAll () {
+            return activitiesRepository.findAll();
         }
-        activitiesRepository.deleteById(activitiesId);
-    }
 
+
+        //pecriso do metodo de edition para pegar users
+        @GetMapping("/editions/{editionId}/activities/{activitiesId}")
+        public Activities searchUser ( @PathVariable int activitiesId){
+            return activitiesRepository.findById(activitiesId);
+        }
+
+        @PutMapping("/editions/{editionId}/activities/{activitiesId}")
+        public Activities update ( @PathVariable int activitiesId, @RequestBody Activities a){
+            Activities activities = activitiesRepository.findById(activitiesId);
+            activities.setName(a.getName());
+            //activities.setType(a.getType());
+            activities.setDescription(a.getDescription());
+            activities.setDate(a.getDate());
+            activities.setStart_time(a.getStart_time());
+            activities.setEnd_time(a.getEnd_time());
+
+            return activitiesRepository.save(activities);
+        }
+
+        @DeleteMapping("/editions/{editionId}/activities/{activitiesId}")
+        public void delete ( @PathVariable int activitiesId){
+            public void delete ( @PathVariable int editionId, @PathVariable int activitiesId){
+                Activities a = activitiesRepository.findById(activitiesId)
+                Edition edition = findEditionofActivities(a);
+                if (edition != null) {
+                    edition.getAtividades().remove(a); // Remove a atividade da lista de atividades da edição
+                    editionRepository.save(edition); // Atualiza a edição no banco de dados
+                }
+                activitiesRepository.deleteById(activitiesId);
+            }
+
+        }
+    }
 }
--- api/src/main/java/br/com/projeto/api/controle/SpaceController.java
+++ api/src/main/java/br/com/projeto/api/controle/SpaceController.java
@@ -1,21 +1,46 @@
 package br.com.projeto.api.controle;
 
+import br.com.projeto.api.modelo.Space;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.projeto.api.repositorio.SpaceRepository;
+
+import java.util.Optional;
 
 @RestController
 public class SpaceController {
     
     @Autowired
-    private SpaceRepository SpaceRepository;
+    private SpaceRepository spaceRepository;
 
+    @GetMapping("/spaces")
+    public Iterable<Space> findAll() {
+        return spaceRepository.findAll();
+    }
+
+    @GetMapping("/spaces/{spacesId}")
+    public Optional<Space> findSpace(@PathVariable int spacesId) {
+        return spaceRepository.findById(spacesId);
+    }
+
+    @PostMapping("/spaces")
+    public void save(HttpServletResponse response, @RequestBody Space space){
+        spaceRepository.save(space);
+        response.setStatus(201);
+    }
+
+    @PutMapping("/spaces/{spaceId}")
+    public void update(HttpServletResponse response, @PathVariable("spaceId") int spaceId,  @RequestBody Space space) {
+        space.setId(spaceId);
+        if (!spaceRepository.put(space)) {
+            response.setStatus(400);
+        }
+    }
     @DeleteMapping("/spaces/{spacesId}")
     public void delete(@PathVariable int spacesId) {
-        SpaceRepository.deleteById(spacesId);
+        spaceRepository.deleteById(spacesId);
     }
     
 }
